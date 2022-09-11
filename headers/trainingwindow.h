ae6c2a79c6f802eb211743b346ec2b1bef550b67
Tosiek <antonizieciak@gmail.com>
2022-09-11 14:22:37 +0200

    Requirements in training window are now changing relative to the amount of unit that player want to train.

--- headers/trainingwindow.h
+++ headers/trainingwindow.h
@@ -5,21 +5,33 @@
 #ifndef AGES1_TRAININGWINDOW_H
 #define AGES1_TRAININGWINDOW_H
 
+#include <vector>
+#include <algorithm>
+
 #include "ui/label.h"
 #include "ui/button.h"
+#include "ui/clickable.h"
 
 class TrainingWindow {
 public:
     void create(int nMoney, int nPeople, int nIron, int upgTime, const std::string& unitName,sf::RenderWindow &W);
     void main_loop();
     void handle_event();
+    void do_stuff();
     void display();
 
 private:
     sf::RenderWindow window_;
-    Label sure_;
     sf::Event event_;
     sf::Cursor cursor_;
+
+    Label sure_,count_,counter_;
+    Clickable plus_,minus_;
+
+    int money_,people_,iron_,time_;
+    int quantity_=1;
+
+    void set_captions();
 };
 
 
--- src/trainingwindow.cpp
+++ src/trainingwindow.cpp
@@ -4,23 +4,31 @@
 
 #include "../headers/trainingwindow.h"
 
-void TrainingWindow::create(int nMoney, int nPeople, int nIron, int upgTime, const std::string& unitName,sf::RenderWindow &W){
+void TrainingWindow::create(int nMoney,int nPeople,int nIron,int upgTime,const std::string& unitName,sf::RenderWindow &W){
+    //assigning resources
+    money_=nMoney;
+    people_=nPeople;
+    iron_=nIron;
+    time_=upgTime;
+
     //creating info-window
-    //auto mode=sf::VideoMode(300,450);
-    window_.create(sf::VideoMode(300,450),"Train "+unitName,sf::Style::Titlebar | sf::Style::Close);
+    sf::VideoMode mode=sf::VideoMode(450,300);
+    window_.create(mode,"Train "+unitName,sf::Style::Titlebar | sf::Style::Close);
     sf::Vector2i windowPos=W.getPosition()+sf::Vector2i(250,75);
     window_.setPosition(windowPos);
 
     //creating info to show in upgrade window
-    sure_.create(DEFAULT_FONT, 150, 100, "", 30);
+    sure_.create(DEFAULT_FONT,225,100,"",30);
 
-    std::string firstLine="Required money: "+std::to_string(nMoney)+"\n";
-    std::string secondLine="Required people: "+std::to_string(nPeople)+"\n";
-    std::string thirdLine="Required iron: "+std::to_string(nIron)+"\n";
-    std::string fourthLine="Upgrade time: "+std::to_string(upgTime)+"\n";
-    std::string sureCaption=firstLine+secondLine+thirdLine+fourthLine;
-    sure_.setCaption(sureCaption);
-    sure_.center();
+    //info about quantity of units to train
+    count_.create(DEFAULT_FONT,90,155,"Quantity",30);
+    counter_.create(DEFAULT_FONT,300,163,"1",30);
+    counter_.center();
+    plus_.create_element("Textures/plus.png",sf::Vector2f(320,155));
+    minus_.create_element("Textures/minus.png",sf::Vector2f(250,155));
+
+    //info which is changed relatively to the quantity
+    set_captions();
 
     //starting window
     main_loop();
@@ -31,12 +39,29 @@ void TrainingWindow::handle_event(){
         if(event_.type==sf::Event::Closed){
             window_.close();
         }
+
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            sf::Vector2i mPos=sf::Mouse::getPosition(window_);
+            int mPosX=mPos.x;
+            int mPosY=mPos.y;
+
+            if(plus_.onClick(mPosX,mPosY)){
+                quantity_++;
+                set_captions();
+            }
+
+            if(minus_.onClick(mPosX,mPosY)){
+                quantity_--;
+                set_captions();
+            }
+        }
     }
 }
 
 void TrainingWindow::main_loop() {
     while(window_.isOpen()){
         handle_event();
+        do_stuff();
         display();
     }
 }
@@ -46,5 +71,42 @@ void TrainingWindow::display() {
 
     sure_.show(window_);
 
+    //quantity
+    count_.show(window_);
+    counter_.show(window_);
+    plus_.draw_it(window_);
+    minus_.draw_it(window_);
+
     window_.display();
+}
+
+void TrainingWindow::do_stuff(){
+    auto mPos=sf::Mouse::getPosition(window_);
+    int mPosX=mPos.x;
+    int mPosY=mPos.y;
+
+    std::vector<bool> focused;
+
+    focused.push_back(plus_.onFocus(mPosX,mPosY));
+    focused.push_back(minus_.onFocus(mPosX,mPosY));
+
+    if(std::count(focused.begin(),focused.end(),true)){
+        cursor_.loadFromSystem(sf::Cursor::Hand);
+    }else{
+        cursor_.loadFromSystem(sf::Cursor::Arrow);
+    }
+    window_.setMouseCursor(cursor_);
+}
+
+void TrainingWindow::set_captions(){
+    counter_.setCaption(std::to_string(quantity_));
+    counter_.center();
+
+    std::string firstLine="Required money: "+std::to_string(quantity_*money_)+"\n";
+    std::string secondLine="Required people: "+std::to_string(quantity_*people_)+"\n";
+    std::string thirdLine="Required iron: "+std::to_string(quantity_*people_)+"\n";
+    std::string fourthLine="Upgrade time: "+std::to_string(quantity_*time_)+"\n";
+    std::string sureCaption=firstLine+secondLine+thirdLine+fourthLine;
+    sure_.setCaption(sureCaption);
+    sure_.center();
 }
